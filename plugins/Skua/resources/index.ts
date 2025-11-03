5f6361b41497f6530e4845c19985503a14e9ef00
DarioSacco <dariosacco9@gmail.com>
2025-11-03 18:42:15 +0100

    add line between points; some style

--- plugins/Skua/resources/index.ts
+++ plugins/Skua/resources/index.ts
@@ -1,1 +1,3 @@
 import '@chialab/dna-map-scroller';
+import '@chialab/dna-slideshow';
+import './theme';
--- plugins/Skua/src/View/Helper/MapHelper.php
+++ plugins/Skua/src/View/Helper/MapHelper.php
@@ -13,23 +13,76 @@ use Chialab\Geometry\Geometry;
 class MapHelper extends Helper
 {
     /**
-     * Get a lat/lon string from a location object
+     * Get a lat/lon string from a BEdita object
      *
-     * @param object $location location object
+     * @param object $object BEdita object
      * @return string|null lat/lon string
      */
-    public function getLocationCoords(object|null $location): string|null
+    public function getObjectCoords(object|null $object): string|null
     {
-        if (!$location || !isset($location['coords'])) {
+        if (!$object || !isset($object['coords'])) {
             return null;
         }
 
-        $geom = Geometry::parse($location['coords'])->getGeometry();
+        $geom = Geometry::parse($object['coords'])->getGeometry();
 
         if (!$geom instanceof Point) {
             return null;
         }
 
         return implode(',', [number_format($geom->y(), 6, '.', ''), number_format($geom->x(), 6, '.', '')]);
+    }
+
+    /**
+     * Build a geoJSON object from an object list.
+     *
+     * @param iterable $objects Objects list
+     * @return array GeoJSON response
+     */
+    public function geoJSON(iterable $objects): array
+    {
+        $jsonObject = ['type' => 'FeatureCollection', 'features' => []];
+        $lineCoords = [];
+
+        foreach ($objects as $object) {
+            if (empty($object['coords'])) {
+                continue;
+            }
+
+            $object['marker-symbol'] = 'marker-skua';
+            $coords = Geometry::parse($object['coords']);
+            $geometry = json_encode($coords);
+            $jsonObject['features'][] = [
+                'geometry' => json_decode($geometry, true),
+                'geometry' => $coords,
+                'type' => 'Feature',
+                'properties' => [
+                    'marker-symbol' => 'marker-skua',
+                    'marker-anchor' => 'bottom',
+                ],
+            ];
+            $geom = $coords->getGeometry();
+            if ($geom instanceof Point) {
+                $lineCoords[] = [$geom->x(), $geom->y()];
+            }
+        }
+
+        $jsonObject['features'][] = [
+            'type' => 'Feature',
+            'geometry' => [
+                'type' => 'LineString',
+                'coordinates' => $lineCoords,
+            ],
+            'properties' => [
+                'style' => [
+                    'type' => 'line',
+                    'line-color' => '#ff0000',
+                    'line-width' => 2,
+                    'line-dasharray' => [8, 8],
+                ],
+            ]
+        ];
+
+        return $jsonObject;
     }
 }
