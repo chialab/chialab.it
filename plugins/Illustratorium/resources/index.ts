5285a197fba133e06d0ebe2dad33c04652addf91
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2025-12-23 13:03:07 +0100

    Fix zoom for illustrators galleries

--- plugins/Illustratorium/resources/index.ts
+++ plugins/Illustratorium/resources/index.ts
@@ -2,7 +2,7 @@ import { openModal } from '@chialab/cdk';
 import { delegateEventListener } from '@chialab/dna';
 
 delegateEventListener(document.body, 'click', 'img[data-modal]', (event, target) => {
-    const parent = (target as HTMLElement).parentElement as HTMLElement;
+    const parent = (target as HTMLElement).closest('.illustrator-gallery') as HTMLElement;
     const group = (target as HTMLElement).dataset.modal;
     const sources = Array.from(parent.querySelectorAll(`img[data-modal="${group}"]`)).map(
         (element) => (element as HTMLImageElement).src
